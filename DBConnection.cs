994557d86e69aebb0e9878434c3012da223ef876
HanhPhucCong <126133472+HanhPhucCong@users.noreply.github.com>
2023-03-22 20:25:15 +0700

    Add files via upload

---
+++ DBConnection.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Data.SqlClient;
+namespace EnglishCentreManagement.Database
+{
+    public class DBConnection
+    {
+        public static DataTable getData(SqlConnection conn, string sqlStr)
+        {
+            try
+            {
+                conn.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter(sqlStr, conn);
+                DataTable dtSinhVien = new DataTable();
+                adapter.Fill(dtSinhVien);
+                return dtSinhVien;
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            finally
+            {
+                conn.Close();
+            }
+#pragma warning disable CS8603 // Possible null reference return.
+            return null;
+#pragma warning restore CS8603 // Possible null reference return.
+        }
+
+        public static void ThucThi(SqlConnection conn, string sqlStr)
+        {
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(sqlStr, conn);
+                if (cmd.ExecuteNonQuery() > 0)
+                {
+                    MessageBox.Show("Thuc thi thanh cong");
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Thuc thi that bai" + ex);
+            }
+
+            finally
+            {
+                conn.Close();
+            }
+        }
+    }
+
+}

c2eaafa191e54a9d62b7ce5608e7614c6a266610
cong09112003 <congnguyenhuu2703@gmail.com>
2023-05-17 02:04:37 +0700

    hoan thanh add,edit,delet teacher, student vs hoan thien schedule

--- Final_Project_Group5/EnglishCentreManagement/Database/StudentDAO.cs
+++ Final_Project_Group5/EnglishCentreManagement/Database/StudentDAO.cs
@@ -27,7 +27,7 @@ namespace EnglishCentreManagement.Database
 
         public void Update(Student Stu)
         {
-            string str = string.Format("UPDATE HOCVIEN SET TenHocVien = '{0}', NgaySinh = '{1}', GioiTinh = '{2}', DiaChi = '{3}', SoDienThoai ='{4}', ChungMinhNhanDan ='{5}', SoTaiKhoan = '{6}', RankLevel = '{7}' WHERE  MaHocVien = '{8}'", Stu.NamePerson, Stu.DateBorn, Stu.Gender, Stu.Address, Stu.PhoneNum, Stu.IdentityCard, Stu.BankNumber, Stu.RankLevel, Stu.Enter_Infor.ID);
+            string str = string.Format("UPDATE HOCVIEN SET TenHocVien = N'{0}', NgaySinh = '{1}', GioiTinh = N'{2}', DiaChi = N'{3}', SoDienThoai ='{4}', ChungMinhNhanDan ='{5}', SoTaiKhoan = '{6}', RankLevel = '{7}' WHERE  MaHocVien = '{8}'", Stu.NamePerson, Stu.DateBorn, Stu.Gender, Stu.Address, Stu.PhoneNum, Stu.IdentityCard, Stu.BankNumber, Stu.RankLevel, Stu.Enter_Infor.ID);
             DBConnection.Execute(conn, str);
         }
 
